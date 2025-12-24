fd4f52a1e66e0c7c2bedf2e258cdb18034e4b95a
Sneha Poojary <snehapoojary2004@gmail.com>
2025-12-24 22:12:54 +0530

    Add user authentication methods: password hashing, token generation, and pre-save middleware

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -4,3 +4,28 @@ import { User } from "../models/user.model.js";
 import { uploadToCloudinary } from "../utils/cloudinary.js";
 import {ApiResponse} from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken";
+
+const generateAccessAndRefreshToken = async (userId)=>{
+
+    try {
+        const user= await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({validateBeforeSave: false});
+
+    } catch (error) {
+        throw new ApiError(500,"Something Went Wrong while generating refresh and access token")
+    }
+}
+
+
+export { generateAccessAndRefreshToken}
+// generate access and refresh token
+// register user
+// login user
+// logout user
+// change current password
+// get current user
+// change current profile pic 
